7fd1ec57f37586bfb70584c87372929d1c60ce0c
Seung won <laurent01@unist.ac.kr>
2020-08-08 13:43:53 +0900

    Initial Commit

---
+++ DotProduct_Test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DotProduct_Test
+//
+//  Created by Seung won Yoo on 2020/08/08.
+//  Copyright © 2020 Seung won Yoo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
